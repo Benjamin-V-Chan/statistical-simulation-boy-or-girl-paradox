6fdb5bc4f4d0008d6df02ab464b41072cc30fd44
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-10 00:08:52 -0700

    05_compare_strategies script outline

---
+++ scripts/05_compare_strategies.py
@@ -0,0 +1,4 @@
+# Compares different stopping strategies numerically
+# Compute the average number of children per family under each strategy
+# Compare expected vs. actual birth ratios
+# Store comparisons in a CSV file
