2d7a8f827542754968fd8ce77eacf86968cc1493
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-10 00:08:11 -0700

    04_statistical_tests script outline

---
+++ scripts/04_statistical_tests.py
@@ -0,0 +1,4 @@
+# Performs statistical significance tests
+# 1. Load gender ratio data
+# 2. Conduct chi-square tests to compare observed vs. expected distributions
+# 3. Save test results in a text file
